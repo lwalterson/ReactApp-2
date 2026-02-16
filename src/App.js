2a8d6b033309b7467352b9096d21972a73263349
lwalterson <laurenbwalterson@gmail.com>
2026-02-16 00:04:34 -0500

    Refactor App component to use Header, Content, and Footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import './App.css';
-import Sidebar from './components/Sidebar';
+// import logo from './logo.svg';
+import  './App.css';
+import {Heads} from './components/Header';
+import {Cont} from './components/Content';
+import {Foot} from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Sidebar 1</p>
-        <Sidebar initialMenuItems={[
-            'Albert Eggstein',
-            'Attila the Hen',
-            'Dixie Chick',
-            'Gregory Peck',
-            'Mary Poopins'
-          ]}></Sidebar>
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+    <div>
+      <Heads />
+      <Cont />
+      <Foot />
+    </div>
       </header>
     </div>
   );
